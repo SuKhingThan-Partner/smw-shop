7d08fd8ce5cbd7623707f663ad8601dc77bf99ae
ASUS <sukhingthan92@gmail.com>
2021-11-17 23:02:21 +0630

    bottomNav

--- lib/components/custom_bottom_nav_bar.dart
+++ lib/components/custom_bottom_nav_bar.dart
@@ -1,73 +1,76 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:get/get.dart';
 import 'package:smw_shop_backend/helper/enums.dart';
 import 'package:smw_shop_backend/helper/style.dart';
-import 'package:smw_shop_backend/views/login/login_screen.dart';
 
-class CustomBottomNavBar extends StatelessWidget{
-  const CustomBottomNavBar({
-    Key? key,
-    required this.selectedMenu
-}): super(key:key);
+class CustomBottomNavBar extends StatelessWidget {
+  const CustomBottomNavBar({Key? key, required this.selectedMenu})
+      : super(key: key);
 
   final MenuState selectedMenu;
-
+  void onTabTapped(int index){
+    setState(){
+     int _currentIndex=index;
+    }
+  }
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     final Color inActiveIconColor = Color(0xFFB6B6B6);
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 10),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            offset: Offset(0,-15),
-            blurRadius: 20,
-            color: Color(0xFFDADADA).withOpacity(0.15),
-          )
-        ],
-        // borderRadius: BorderRadius.only(
-        //   topLeft: Radius.circular(40),
-        //   topRight: Radius.circular(40),
-        // )
-      ),
-      child: SafeArea(
-        top:false,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            IconButton(onPressed: () {
-              Get.off(() => LoginPage());
-
-            }, icon: SvgPicture.asset("assets/icons/ShopIcon.svg",
-            color: MenuState.home == selectedMenu?
-              kPrimaryColor: inActiveIconColor,)),
-            IconButton(onPressed: () {
-              Get.off(() => LoginPage());
-
-            }, icon: SvgPicture.asset("assets/icons/Heart Icon.svg",
-              color: MenuState.favourite == selectedMenu?
-              kPrimaryColor: inActiveIconColor,)),
-            IconButton(onPressed: () {
-              Get.off(() => LoginPage());
-
-            }, icon: SvgPicture.asset("assets/icons/Chat bubble Icon.svg",
-              color: MenuState.message == selectedMenu?
-              kPrimaryColor: inActiveIconColor,)),
-            IconButton(onPressed: () {
-              Get.off(() => LoginPage());
-
-            }, icon: SvgPicture.asset("assets/icons/User Icon.svg",
-              color: MenuState.profile == selectedMenu?
-              kPrimaryColor: inActiveIconColor,))
-
-
-          ],
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      selectedItemColor: Colors.red,
+      unselectedItemColor: Colors.black45,
+      currentIndex: 3,
+      items: [
+        BottomNavigationBarItem(
+          icon: SvgPicture.asset(
+            "assets/icons/ShopIcon.svg",
+            color: MenuState.profile == selectedMenu
+                ? kPrimaryColor
+                : inActiveIconColor,
+          ),
+          title: new Text(
+            "Home",
+            //style: TextStyle(color: Colors.black45),
+          ),
         ),
+        BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/icons/Heart Icon.svg",
+              color: MenuState.favourite == selectedMenu
+                  ? kPrimaryColor
+                  : inActiveIconColor,
+            ),
+            title: new Text(
+              "Favourite",
+              //style: TextStyle(color: Colors.black45),
+            )),
+        BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/icons/Chat bubble Icon.svg",
+              color: MenuState.message == selectedMenu
+                  ? kPrimaryColor
+                  : inActiveIconColor,
+            ),
+            title: new Text(
+              "Message",
+              // style: TextStyle(color: Colors.black45),
+            )),
+        BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/icons/User Icon.svg",
+              color: MenuState.profile == selectedMenu
+                  ? kPrimaryColor
+                  : inActiveIconColor,
+            ),
+            title: new Text("Profile"
+                //style: TextStyle(color: Colors.black45),
+                )),
 
-      ),
+      ],
+      onTap: onTabTapped,
     );
+
   }
 }
